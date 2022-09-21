e247a6dbe248dfd8639c1989f5d6e6aea3258d4a
Aleksei Naumov <naumov2407@gmail.com>
2022-09-20 22:17:06 -0700

    lesson 1 done with out *

--- todo/users/models.py
+++ todo/users/models.py
@@ -1,8 +1,9 @@
+# from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
 
 class User(models.Model):
-    user_name = models.CharField(max_length=64)
+    user_name = models.CharField(max_length=64, unique=True,)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
-    email = models.CharField(max_length=64, unique=True)
+    email = models.EmailField(unique=True)
